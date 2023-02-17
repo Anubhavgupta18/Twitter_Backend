136d62ef6f90c503b2c4ae63dda1871b57bdf050
anubhav <anubhavshivahre@gmail.com>
2023-02-17 17:24:04 +0530

    added tweet service with hashtag processing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const connect = require('./configs/dbconfig');
-const Comment = require('./models/comment');
-
-const tweetrepo = require('./repository/tweet-repository'); 
 
 
 const app = express();
@@ -11,7 +8,5 @@ app.listen(3000, async () => {
     console.log('server started on PORT:3000'); 
     await connect();
     console.log('Mongodb server connected');
-    const Tweet = new tweetrepo();
-    const tweet = await Tweet.create({content:'This tweet is triggered using hooks'});
-    console.log(tweet);
+
 });
---
+++ src/services/tweet-service.js
@@ -0,0 +1,50 @@
+const { TweetRepository, HashtagRepository } = require("../repository/index");
+
+
+class TweetService{
+    constructor() {
+        this.tweetRepository = new TweetRepository();
+        this.hashtagRepository = new HashtagRepository();
+    }
+
+    async create(data)
+    {
+        try {
+            const content = data.content;
+
+            //using a regular expression we are filtering hashtags
+            const tags = content.match(/#[a-zA-Z0-9_]+/g)
+                .map((tag) => tag.substring(1).toLowerCase());
+            
+            const tweet = await this.tweetRepository.create(data);
+            
+            //passing title of tags array inside findByName to get already present tags inside collect Hashtag
+            let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
+
+            //filtering the array of objects to get title of tags
+            const titleOfAlreadyPresentTags = alreadyPresentTags.map((tag) => tag.title);
+
+            //getting title of tags which are not present inside collection
+            let newTags = tags.filter((tag) => !(titleOfAlreadyPresentTags.includes(tag)));
+
+            //creating hashtag object from titles
+            newTags = newTags.map((tag) => {
+                return { title: tag, tweets: [tweet.id] }
+            });
+
+            await this.hashtagRepository.bulkCreate(newTags);
+
+            //adding tweetId inside tweets array which were already present inside collection
+            alreadyPresentTags.forEach((tag) => {
+                tag.tweets.push(tweet.id);
+                tag.save();
+            });
+
+            return tweet; 
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+module.exports = TweetService;
