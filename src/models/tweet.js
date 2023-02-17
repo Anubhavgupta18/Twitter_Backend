44ec5602a5b165d278826fec2be6a1fc6c3421d6
anubhav <anubhavshivahre@gmail.com>
2023-02-17 13:12:00 +0530

    added hashtag model

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -3,38 +3,39 @@ const mongoose = require('mongoose');
 const tweetSchema = new mongoose.Schema({
     content: {
         type: String,
-        required: true
+        required: true,
+        max:[250,'Tweet content can have max. 250 characters']
     },
-    userEmail: {
-        type: String
-    },
-    comments: [
+    
+    hashtags: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comment'
+            ref: 'Hashtag'
         }
     ]
 }, { timestamps: true });
 
-//virtuals
-tweetSchema.virtual('getWithUserEmail').get(function process() {
-    return `${this.content}\n${this.userEmail}`;
-});
 
-//HOOKS
-
-//Use Case of Hooks:
-//We can use hooks in a scenario where a user is deleting his/her account then all the tweets,
-//asociated with his/her account will also be deleted using a post hooks
-tweetSchema.pre('save', function (next) {
-    //based on the no. of arguments it decides that the hook is synchronous or asynchronous
-    console.log('Inside Hooks');
-    this.content = this.content + '....(content was modified)'
-    next();
-}); 
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
 
 module.exports = Tweet;
 
 //INDEXES
+
+//virtuals
+// tweetSchema.virtual('getWithUserEmail').get(function process() {
+//     return `${this.content}\n${this.userEmail}`;
+// });
+
+//HOOKS
+
+//Use Case of Hooks:
+//We can use hooks in a scenario where a user is deleting his/her account then all the tweets,
+//asociated with his/her account will also be deleted using a post hooks
+// tweetSchema.pre('save', function (next) {
+//     //based on the no. of arguments it decides that the hook is synchronous or asynchronous
+//     console.log('Inside Hooks');
+//     this.content = this.content + '....(content was modified)'
+//     next();
+// }); 
