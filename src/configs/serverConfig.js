6c4239f152d8ab495d77a032966d08c43a07c152
anubhav <anubhavshivahre@gmail.com>
2023-03-10 16:45:42 +0530

    added image upload functionality using aws s3 and multer

--- src/configs/serverConfig.js
+++ src/configs/serverConfig.js
@@ -4,8 +4,16 @@ dotenv.config();
 const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
 const secretKey = process.env.secretKey;
+const AWS_REGION = process.env.AWS_REGION;
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+const BUCKET_NAME = process.env.BUCKET_NAME; 
 export {
     PORT,
     MONGO_URL,
-    secretKey
+    secretKey,
+    AWS_REGION,
+    AWS_SECRET_ACCESS_KEY,
+    AWS_ACCESS_KEY_ID,
+    BUCKET_NAME
 };
--- src/controllers/tweet-controller.js
+++ src/controllers/tweet-controller.js
@@ -1,16 +1,29 @@
 import TweetService from '../services/tweet-service.js';
+import upload from "../configs/file-upload-s3-config.js";
 
 const tweetService = new TweetService();
 
-const createTweet = async (req,res) => {
+const singleUploader = upload.single('image');
+
+
+const createTweet = async (req, res) => {
     try {
-        const tweet = await tweetService.create(req.body);
-        return res.status(201).json({
-            data: tweet,
-            success: true,
-            message:'Tweet created succesfully',
-            err:{}
-        });
+        singleUploader(req, res, async function (err, data) {
+            if (err) {
+                return res.status(500).json({
+                    error:err
+                });
+            }
+            const payload = { ...req.body };
+            payload.image = req.file.location;
+            const tweet = await tweetService.create(payload);
+            return res.status(201).json({
+                data: tweet,
+                success: true,
+                message: 'Tweet created succesfully',
+                err: {}
+            });
+        })
     } catch (error) {
         console.log(error);
         return res.status(400).json({
@@ -22,14 +35,14 @@ const createTweet = async (req,res) => {
     }
 }
 
-const getTweet = async (req,res) => {
+const getTweet = async (req, res) => {
     try {
         const tweet = await tweetService.getTweet(req.params.id);
         return res.status(201).json({
             data: tweet,
             success: true,
-            message:'Tweet fetched succesfully',
-            err:{}
+            message: 'Tweet fetched succesfully',
+            err: {}
         });
     } catch (error) {
         console.log(error);
